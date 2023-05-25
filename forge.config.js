e4d8c7be95bcacd6ab88abebdd7b060f95c8f753
Oraange <hrpp1300@naver.com>
2023-05-25 11:52:11 +0900

    Feat: Ready to deploy

--- forge.config.js
+++ forge.config.js
@@ -1,10 +1,36 @@
 module.exports = {
-  packagerConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'Oraange',
+          name: 'electron-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
+  packagerConfig: {
+    // For MacOS
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdpassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        // For Windows
+        // certificateFile: './cert.pfx',
+        // certificatePassword: process.env.CERTIFICATE_PASSWORD
+      },
     },
     {
       name: '@electron-forge/maker-zip',
