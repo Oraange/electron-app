23d18de42d7a9a428972bc91521a2a00c764e0f9
Oraange <hrpp1300@naver.com>
2023-06-14 18:53:32 +0900

    feat: Add ffmpeg features

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,11 +1,13 @@
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('ffmpeg-static-electron').path;
 
+// ffmpeg binary 파일의 위치 설정
 ffmpeg.setFfmpegPath(ffmpegPath);
 
+// 영상 효과 추가
 function fadeInOut(inputFile, outputFile) {
     return new Promise((resolve, reject) => {
-        document.getElementById('fadeEffect').innerHTML = "🌘Fade In / Out 효과를 넣고있어요"
+        document.getElementById('process').innerHTML = "🌘Fade In / Out 효과를 넣고있어요"
         ffmpeg(inputFile)
         .videoFilter('fade=t=in:st=0:d=10,fade=t=out:st=50:d=5')
         .audioCodec('copy')
@@ -16,9 +18,10 @@ function fadeInOut(inputFile, outputFile) {
     });
 }
 
+// wav 파일 추출
 function convertToWav(inputFile, outputFile) {
     return new Promise((resolve, reject) => {
-        document.getElementById('getWav').innerHTML = "🔉음성을 추출 중이에요"
+        document.getElementById('process').innerHTML = "🔉음성을 추출 중이에요"
         ffmpeg(inputFile)
         .audioCodec('pcm_s16le')
         .audioChannels(1)
@@ -31,9 +34,10 @@ function convertToWav(inputFile, outputFile) {
     })
 }
 
+// 자막 burn in
 function burnInSubtitle(inputVideoFile, inputSubtitleFile, outputFile) {
     return new Promise((resolve, reject) => {
-        document.getElementById('pending').innerHTML = "📽자막을 입히는 중이에요!"
+        document.getElementById('process').innerHTML = "📽자막을 입히는 중이에요!"
         ffmpeg(inputVideoFile)
         .videoFilter(`subtitles=${inputSubtitleFile}:force_style='FontName=맑은 고딕, Fontsize=150px'`)
         .videoCodec('libx264')
@@ -45,4 +49,65 @@ function burnInSubtitle(inputVideoFile, inputSubtitleFile, outputFile) {
     })
 }
 
-module.exports = { fadeInOut, convertToWav, burnInSubtitle };
+// 영상 분리
+function separateVideo(inputVideoFile, sepTime, outputFile1, outputFile2) {
+    const duration = "00:31:25"
+    return new Promise((resolve, reject) => {
+        document.getElementById('process').innerHTML = "🔪영상을 나누고 있습니다"
+        ffmpeg()
+        .input(inputVideoFile)
+        .inputOptions(['-ss', '00:00:00', '-t', sepTime])
+        .output(outputFile1)
+        .outputOptions('-c:v', 'copy', '-c:a', 'copy')
+        .on('end', () => {
+            ffmpeg()
+            .input(inputVideoFile)
+            .inputOptions(['-ss', sepTime, '-t', duration])
+            .output(outputFile2)
+            .outputOptions('-c:v', 'copy', '-c:a', 'copy')
+            .on('end', resolve)
+            .on('error', reject)
+            .run()
+        })
+        .on('error', reject)
+        .run();
+    })
+}
+
+// 이미지로 영상 생성
+function imgToVideo(inputImageFile, outputFile) {
+    return new Promise((resolve, reject) => {
+        document.getElementById('process').innerHTML = "🌠이미지로 영상 생성 중";
+        ffmpeg(inputImageFile)
+        .loop(1)
+        .videoFilter('scale=trunc(iw/2)*2:trunc(ih/2)*2')
+        .videoCodec('libx264')
+        .setDuration(5)
+        .output(outputFile)
+        .on('end', resolve)
+        .on('error', reject)
+        .run()
+    });
+}
+
+// 영상 합치기
+function concatVideos(inputFile, outputFile) {
+    return new Promise((resolve, reject) => {
+        document.getElementById('concat').innerHTML = "👫영상을 합치는 중";
+        ffmpeg()
+        .concat(inputFile)
+        .output(outputFile)
+        .on('end', resolve)
+        .on('error', reject)
+        .run()
+    });
+}
+
+module.exports = { 
+    fadeInOut,
+    convertToWav,
+    burnInSubtitle,
+    separateVideo,
+    imgToVideo,
+    concatVideos
+};
--- renderer.js
+++ renderer.js
@@ -1,14 +1,14 @@
 const electron = require('electron')
-const { convertToWav, burnInSubtitle, fadeInOut } = require('./ffmpeg.js');
+const {
+    convertToWav,
+    burnInSubtitle,
+    fadeInOut,
+    separateVideo,
+    imgToVideo,
+    concatVideos
+} = require('./ffmpeg.js');
 
 const ffmpegPath = require('ffmpeg-static-electron').path;
-
-const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response) // prints out 'pong'
-}
-
-// func()
 
 const dragZone = document.getElementById('drag')
 
@@ -79,24 +79,54 @@ const playVideo = (file, videoPlayer) => {
     videoPlayer.play();
 };
 
-document.querySelector('#getAudio').addEventListener('submit', (event) => {
-    console.time("음성 추출 작업 시간: ");
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= 0.1;
+    }, 80);
+}
+
+const fixedDiv = document.getElementById('process');
+
+document.querySelector('#getAudio').addEventListener('click', (event) => {
     event.preventDefault();
+
+    if (file.files['length'] === 0) {
+        fixedDiv.innerHTML = "아직 영상이 없어요";
+        fixedDiv.style.display = 'block'
+        fixedDiv.style.opacity = 1
+        fade(fixedDiv)
+        return;
+    }
+    console.time("음성 추출 작업 시간: ");
     const { path } = file.files[0];
 
     convertToWav(path, 'media/aplis_output.wav')
     .then(() => {
-        document.getElementById('getWav').innerHTML = "🔊음성 추출이 완료되었어요"
+        document.getElementById('process').innerHTML = "🔊음성 추출이 완료되었어요"
         console.log('Get wav file completed');
         console.timeEnd("음성 추출 작업 시간: ");
     })
     .catch((error) => {
-        document.getElementById('getWav').innerHTML = "❌작업에 실패했어요..."
+        document.getElementById('process').innerHTML = "❌작업에 실패했어요..."
         console.error('Get wav file error: ', error);
     });
 });
 
-document.querySelector('#burnIn').addEventListener('submit', (event) => {
+document.querySelector('#burnIn').addEventListener('click', (event) => {
+    if (file.files['length'] === 0) {
+        fixedDiv.innerHTML = "아직 영상이 없어요";
+        fixedDiv.style.display = 'block'
+        fixedDiv.style.opacity = 1
+        fade(fixedDiv)
+        return;
+    }
     console.time("번인 작업 시간: ");
     event.preventDefault();
     const { path } = file.files[0];
@@ -104,7 +134,7 @@ document.querySelector('#burnIn').addEventListener('submit', (event) => {
 
     burnInSubtitle(path, 'media/aplis.srt', outputFile)
     .then(() => {
-        document.getElementById('pending').innerHTML = "👍영상에 자막이 생겼어요!"
+        document.getElementById('process').innerHTML = "👍영상에 자막이 생겼어요!"
         console.log('Burn In completed');
         const videoPlayer = document.getElementById('videoPlayer');
         videoPlayer.src = outputFile;
@@ -113,12 +143,19 @@ document.querySelector('#burnIn').addEventListener('submit', (event) => {
         console.timeEnd("번인 작업 시간: ");
     })
     .catch((error) => {
-        document.getElementById('pending').innerHTML = "❌작업에 실패했어요..."
+        document.getElementById('process').innerHTML = "❌작업에 실패했어요..."
         console.error('Burn In error: ', error);
     });
 });
 
-document.querySelector('#inputFade').addEventListener('submit', (event) => {
+document.querySelector('#inputFade').addEventListener('click', (event) => {
+    if (file.files['length'] === 0) {
+        fixedDiv.innerHTML = "아직 영상이 없어요";
+        fixedDiv.style.display = 'block'
+        fixedDiv.style.opacity = 1
+        fade(fixedDiv)
+        return;
+    }
     console.time("영상 효과 작업 시간: ");
     event.preventDefault();
     const { path } = file.files[0];
@@ -126,7 +163,7 @@ document.querySelector('#inputFade').addEventListener('submit', (event) => {
 
     fadeInOut(path, outputFile)
     .then(() => {
-        document.getElementById('fadeEffect').innerHTML = "🌕영상에 Fade 효과가 적용됐어요"
+        document.getElementById('process').innerHTML = "🌕영상에 Fade 효과가 적용됐어요"
         console.log('Inser FadeIn or FadeOut effect success.')
         const videoPlayer = document.getElementById('videoPlayer');
         videoPlayer.src = outputFile;
@@ -135,7 +172,74 @@ document.querySelector('#inputFade').addEventListener('submit', (event) => {
         console.timeEnd("영상 효과 작업 시간: ");
     })
     .catch((error) => {
-        document.getElementById('fadeEffect').innerHTML = "❌작업에 실패했어요..."
+        document.getElementById('process').innerHTML = "❌작업에 실패했어요..."
         console.error('Insert FadeIn or FadeOut effect is in error: ', error);
     });
 });
+
+document.querySelector('#divide').addEventListener('click', (event) => {
+    if (file.files['length'] === 0) {
+        fixedDiv.innerHTML = "아직 영상이 없어요";
+        fixedDiv.style.display = 'block'
+        fixedDiv.style.opacity = 1
+        fade(fixedDiv)
+        return;
+    }
+    console.time("영상 분리 시간: ");
+    event.preventDefault();
+    const { path } = file.files[0];
+    const outputFile1 = 'media/aplis_sep_1.mp4';
+    const outputFile2 = 'media/aplis_sep_2.mp4';
+
+    separateVideo(path, '00:1:15', outputFile1, outputFile2)
+    .then(() => {
+        document.getElementById('videoSep').innerHTML = "✔️영상 분리에 성공했습니다"
+        console.timeEnd("영상 분리 시간: ");
+        const videoPlayer1 = document.getElementById('videoPlayer1');
+        const videoPlayer2 = document.getElementById('videoPlayer2');
+        videoPlayer1.src = outputFile1;
+        videoPlayer2.src = outputFile2;
+        videoPlayer1.load();
+        videoPlayer1.play();
+        videoPlayer1.addEventListener('ended', () => {
+            videoPlayer2.load();
+            videoPlayer2.play();
+        });
+    })
+    .catch((error) => {
+        document.getElementById('videoSep').innerHTML = "영상 분리에 실패하였습니다";
+        console.error('영상 분리에 실패하였습니다: ', error)
+    })
+});
+
+document.querySelector('#imgToVideo').addEventListener('click', (event) => {
+    const file = document.getElementById('imgFile')
+    if (file.files['length'] === 0) {
+        fixedDiv.innerHTML = "아직 영상이 없어요";
+        fixedDiv.style.display = 'block'
+        fixedDiv.style.opacity = 1
+        fade(fixedDiv)
+        return;
+    }
+    event.preventDefault();
+    console.time("영상 생성 시간: ")
+    const { path } = file.files[0];
+    console.log(path)
+    const outputFile = 'media/img_to_video.mp4'
+
+    imgToVideo(path, outputFile)
+    .then(() => {
+        document.getElementById('boxOfImgToVideo').innerHTML = "👏🏻영상 생성 성공"
+        const videoPlayer3 = document.getElementById('videoPlayer3');
+        videoPlayer3.src = outputFile;
+        videoPlayer3.load();
+        videoPlayer3.play();
+        console.timeEnd("영상 생성 시간: ")
+    })
+    .catch((error) => {
+        document.getElementById('boxOfImgToVideo').innerHTML = "❌영상 생성 실패"
+        console.error('영상 생성에 실패했습니다: ', error)
+    })
+})
+
+document.querySelector('#concat').addEventListener('click')
